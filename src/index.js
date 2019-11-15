6d77db014f80b426d958b4c8ae273844d6a47459
shaktigurung <mr.shakti.gurung@gmail.com>
2019-11-15 20:04:27 +1100

    JSX exercise

--- src/index.js
+++ src/index.js
@@ -4,16 +4,24 @@ import ReactDOM from 'react-dom';
 
 //Create a React Component
 
+// const createButtonText = () => {
+//     return "Click on Me!";
+// }
+
 const App = () => {
+    const buttonText = {text: "Click Me!"};
+    const labelText = "Enter the name :";
+
     return (
     <div> 
-        <label  className= "label" for="name"> Enter the name : </label>
+        <label  className= "label" htmlFor="name">{labelText}</label>
         <input id="name" type="text" />
-        <button style={{backgroundColor:"blue", color: "white"}}> Submit </button>
+        <button style={{backgroundColor:"blue", color: "white"}}> {buttonText.text} </button>
     </div>
     );
 };
 
+// Displaying React Component into the browser
 ReactDOM.render(
     <App />,
     document.querySelector('#root')
